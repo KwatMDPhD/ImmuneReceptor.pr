afe554d9b951710692b1f9132fc22d54988ff5ce
KwatMDPhD <kwat.me@icloud.com>
2025-09-02 16:29:42 -0600

    Motifing

--- src/ImmuneReceptor.jl
+++ src/ImmuneReceptor.jl
@@ -10,46 +10,161 @@ const OU = joinpath(PK, "ou")
 
 using ProgressMeter: @showprogress
 
-function make(s1, s2)
+using Nucleus
+
+# =============================================================================================== #
+# Read
+# =============================================================================================== #
+
+function is_t_gene(st::AbstractString)
+
+    startswith(st, 'T')
+
+end
+
+function is_t_gene(::Any)
+
+    false
+
+end
+
+function is_cdr3(st)
+
+    st[1] === 'C' && st[end] === 'F'
+
+end
+
+# =============================================================================================== #
+# VJ
+# =============================================================================================== #
+
+function make_vj(vg_, jg_)
+
+    u1_ = unique(vg_)
+
+    u2_ = unique(jg_)
+
+    d1 = Dict(un => nd for (nd, un) in enumerate(u1_))
+
+    d2 = Dict(un => nd for (nd, un) in enumerate(u2_))
+
+    U = zeros(Int, lastindex(u1_), lastindex(u2_))
+
+    for nd in eachindex(vg_)
+
+        U[d1[vg_[nd]], d2[jg_[nd]]] += 1
+
+    end
+
+    u1_, u2_, U
+
+end
+
+function write_vj(ht, vg_, jg_, U)
+
+    Nucleus.HeatPlot.writ(
+        ht,
+        vg_,
+        jg_,
+        U,
+        Dict(
+            # TODO: Size
+            "yaxis" => Dict("title" => Dict("text" => "V gene")),
+            "xaxis" => Dict("title" => Dict("text" => "J gene")),
+        ),
+    )
+
+end
+
+# =============================================================================================== #
+# CDR3
+# =============================================================================================== #
+
+function make_hamming_distance(s1, s2)
 
     sum(c1 != c2 for (c1, c2) in zip(s1, s2))
 
 end
 
-for (s1, s2, re) in (
-    ("AA", "AA", 0),
-    ("AA", "BA", 1),
-    ("AA", "AB", 1),
-    ("AAA", "AAA", 0),
-    ("AAA", "BAA", 1),
-    ("AAA", "BBA", 2),
-    ("AAA", "BBB", 3),
-    ("AAA", "ABB", 2),
-    ("AAA", "AAB", 1),
-)
-
-    @assert make(s1, s2) === re
+function make_todo_distance(s1, s2)
 
 end
 
-function make(un_)
+function make_distance(st_)
 
-    # TODO: Preallocate
-    ha_ = Int[]
+    um = lastindex(st_)
 
-    @showprogress for s1 in un_, s2 in un_
+    di_ = Vector{Int}(undef, div(um * (um - 1), 2))
 
-        if s1 == s2 || lastindex(s1) != lastindex(s2)
+    i1 = 0
 
-            continue
+    @showprogress for i2 in 1:um, i3 in (i2 + 1):um
+
+        s1 = st_[i2]
+
+        s2 = st_[i3]
+
+        if lastindex(s1) == lastindex(s2)
+
+            di_[i1 += 1] = make_hamming_distance(s1, s2)
 
         end
 
-        push!(ha_, make(s1, s2))
+    end
+
+    resize!(di_, i1)
+
+end
+
+# =============================================================================================== #
+# Motif
+# =============================================================================================== #
+
+function get_motif(c1, u1)
+
+    mo_ = String[]
+
+    c2 = c1[4:(end - 3)]
+
+    u2 = lastindex(c2)
+
+    i1 = 0
+
+    i2 = i1 + u1 - 1
+
+    while i2 < u2
+
+        push!(mo_, c2[(i1 += 1):(i2 += 1)])
 
     end
 
-    ha_
+    mo_
+
+end
+
+function get_motif(cd_, u1, u2)
+
+    mo__ = map(cd -> get_motif(cd, u1), cd_)
+
+    di = Dict{String, Vector{Int}}()
+
+    for nd in eachindex(mo__)
+
+        for mo in mo__[nd]
+
+            if !haskey(di, mo)
+
+                di[mo] = Int[]
+
+            end
+
+            push!(di[mo], nd)
+
+        end
+
+    end
+
+    [st for (st, in_) in di if u2 <= lastindex(in_)]
 
 end
 
--- test/1.jl
+++ test/1.jl
@@ -4,20 +4,18 @@ using ImmuneReceptor
 
 # ---- #
 
-const S1_ = String[]
+const V1_ = String[]
 
-const S2_ = String[]
+const J1_ = String[]
 
-const S3_ = String[]
+const C1_ = String[]
 
-for fa in (
-    joinpath(ImmuneReceptor.IN, "gliph", "db", fa) for fa in (
-        "rubelt-naive-CD4.fa",
-        "rubelt-naive-CD8.fa",
-        "tcrab-naive-refdb.fa",
-        "warren-naive.fa",
-    )
-)
+for fa in (joinpath(ImmuneReceptor.IN, "gliph", "db", fa) for fa in (
+    #"rubelt-naive-CD4.fa",
+    #"rubelt-naive-CD8.fa",
+    #"tcrab-naive-refdb.fa",
+    "warren-naive.fa",
+))
 
     for s1 in eachsplit(read(fa, String), '>'; keepempty = false)
 
@@ -27,13 +25,13 @@ for fa in (
 
         @assert s3[end] === 'F'
 
-        push!(S3_, s3)
+        push!(C1_, s3)
 
         s4, s5, _ = split(s2, ','; limit = 3)
 
-        push!(S1_, s4)
+        push!(V1_, s4)
 
-        push!(S2_, s5)
+        push!(J1_, s5)
 
     end
 
@@ -41,60 +39,81 @@ end
 
 # ---- #
 
-const U1_ = unique(S1_)
+const V2_::Vector{String}, J2_::Vector{String}, C2_ = eachcol(
+    filter!(
+        an_ ->
+            ImmuneReceptor.is_t_gene(an_[1]) &&
+                ImmuneReceptor.is_t_gene(an_[2]) &&
+                ImmuneReceptor.is_cdr3(an_[3]),
+        Nucleus.Table.rea(
+            joinpath(ImmuneReceptor.IN, "su", "41586_2017_BFnature22976_MOESM2_ESM.xlsx"),
+            "Raw",
+        )[
+            !,
+            ["TCRBV", "TCRBJ", "CDR-H3"],
+        ],
+    ),
+)
 
-const U2_ = unique(S2_)
+# ---- #
 
-const D1 = Dict(st => nd for (nd, st) in enumerate(U1_))
+for (nd, (vg_, jg_, U)) in
+    ((1, ImmuneReceptor.make_vj(V1_, J1_)), (2, ImmuneReceptor.make_vj(V2_, J2_)))
 
-const D2 = Dict(st => nd for (nd, st) in enumerate(U2_))
-
-const I = zeros(Int, lastindex(U1_), lastindex(U2_))
-
-for nd in eachindex(S1_)
-
-    I[D1[S1_[nd]], D2[S2_[nd]]] += 1
+    ImmuneReceptor.write_vj(joinpath(ImmuneReceptor.OU, "$nd.vj.html"), vg_, jg_, U)
 
 end
 
-Nucleus.HeatPlot.writ(
-    joinpath(ImmuneReceptor.OU, "1.html"),
-    U1_,
-    U2_,
-    I,
+# ---- #
+
+Nucleus.Plotly.writ(
+    joinpath(ImmuneReceptor.OU, "length.html"),
+    (
+        Dict(
+            "name" => 1,
+            "type" => "histogram",
+            "x" => map(lastindex, rand(C1_, lastindex(C2_))),
+        ),
+        Dict("name" => 2, "type" => "histogram", "x" => map(lastindex, C2_)),
+    ),
     Dict(
-        "title" => Dict("text" => "$(lastindex(S1_)) TCRs"),
-        "yaxis" => Dict("title" => Dict("text" => "7 V gene")),
-        "xaxis" => Dict("title" => Dict("text" => "7 J gene")),
+        "yaxis" => Dict("title" => Dict("text" => "Count")),
+        "xaxis" => Dict("title" => Dict("text" => "Length")),
     ),
 )
+
+# ---- #
+
+const U1_ = unique(C1_)
+
+const U2_ = unique(C2_)
+
+# ---- #
+
+const D1_ = ImmuneReceptor.make_distance(U1_)
+
+const D2_ = ImmuneReceptor.make_distance(U2_)
 
 # ---- #
 
 Nucleus.Plotly.writ(
-    joinpath(ImmuneReceptor.OU, "2.html"),
-    (Dict("type" => "histogram", "x" => map(lastindex, S3_)),),
+    joinpath(ImmuneReceptor.OU, "distance.html"),
+    (
+        Dict("name" => 1, "type" => "histogram", "x" => rand(D1_, lastindex(D2_))),
+        Dict("name" => 2, "type" => "histogram", "x" => D2_),
+    ),
     Dict(
         "yaxis" => Dict("title" => Dict("text" => "Count")),
-        "xaxis" => Dict("title" => Dict("text" => "Number of amino acids in a CDR3")),
+        "xaxis" => Dict("title" => Dict("text" => "Distance")),
     ),
 )
 
 # ---- #
 
-const UN_ = unique(S3_)
+const M1_ = ImmuneReceptor.get_motif(C1_, 2, 3)
+
+const M2_ = ImmuneReceptor.get_motif(C2_, 2, 3)
 
 # ---- #
 
-const HA_ = ImmuneReceptor.make(UN_)
 
-Nucleus.Plotly.writ(
-    joinpath(ImmuneReceptor.OU, "3.html"),
-    (Dict("type" => "histogram", "x" => HA_),),
-    Dict(
-        "yaxis" => Dict("title" => Dict("text" => "Count")),
-        "xaxis" => Dict("title" => Dict("text" => "Hamming distance between CDR3s")),
-    ),
-)
-
-# ---- #
