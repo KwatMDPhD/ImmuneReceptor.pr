2f5065bb37664edefc739252ca980c489755dd01
KwatMDPhD <kwat.me@icloud.com>
2025-09-01 21:37:44 -0600

    Structured 2.jl.

--- test/2.jl
+++ test/2.jl
@@ -3,4 +3,22 @@ using Nucleus
 using ImmuneReceptor
 
 # ---- #
+# Read raw file
 
+# ---- #
+# Write table
+
+# ---- #
+# Read table
+
+# ---- #
+# Do global
+
+# ---- #
+# Do local
+
+# ---- #
+# Group
+
+# ---- #
+# Score
